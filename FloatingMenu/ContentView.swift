3dd4148ef285ba6404c5b05fc0f6ec1c4309c7f1
kk0829 <244098979@qq.com>
2024-02-06 15:03:48 +0800

    init project

---
+++ FloatingMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FloatingMenu
+//
+//  Created by 王干 on 2024/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FloatingMenu/FloatingMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingMenuApp.swift
+//  FloatingMenu
+//
+//  Created by 王干 on 2024/2/6.
+//
+
+import SwiftUI
+
+@main
+struct FloatingMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
